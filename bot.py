23f4ec1b2f732477e7131905910c7a1333ce1b27
KuroShonenJPN <150555141+KuroShonenJPN@users.noreply.github.com>
2025-09-30 19:25:09 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,28 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    await bot.tree.sync()  # zaregistruje slash příkazy
+    print(f"Přihlášen jako {bot.user}")
+
+async def load_cogs():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"cogs.{filename[:-3]}")
+
+async def main():
+    async with bot:
+        await load_cogs()
+        await bot.start(TOKEN)
+
+import asyncio
+asyncio.run(main())       
